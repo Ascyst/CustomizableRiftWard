dad6a8a720b46af96c6569d2dcbb57012712bfd3
Ascyst <rmcascyst@gmail.com>
2023-11-14 01:58:52 -0700

    Refactor code as I was getting recursive JSON serialization errors. ImNuts42 pointed out in the VS discord that I should split the config and variables into their own class, which seemed to fix it.

--- CustomizableRiftWard/CustomizableRiftWard.cs
+++ CustomizableRiftWard/CustomizableRiftWard.cs
@@ -1,41 +1,71 @@
-﻿using System; 
-using Vintagestory.API.Common;  
-using Vintagestory.API.Server;  
-using Vintagestory.API.Client;  
-using Vintagestory.API.Config;  
-using Vintagestory.API.MathTools;   
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.API.Client;
+using Vintagestory.API.Config;
+using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 using HarmonyLib;
 using System.Reflection;
-
-
-
+using Vintagestory.Client.NoObf;
+using Newtonsoft.Json;
+using CustomizableRiftWard;
 
 namespace CustomizableRiftWard
 {
 
-    public class RiftWardConfig : ModSystem
+    public static class RiftWardConfig
     {
-        public static double riftBlockChance = 0.95;
-        public static float maxRange = 150f;
-    }
-    // Create a class that implements the ModSystem interface
-    public class CustomizableRiftWard : ModSystem
-    {
-        public override void Start(ICoreAPI api)
+        public static RiftWardConfigData Loaded = new RiftWardConfigData();
+
+        public class RiftWardConfigData
         {
-            base.Start(api);
+            public double riftBlockChance = 0.95;
+            public float maxRange = 30f;
         }
 
+
     }
+
+
+    public class CustomizableRiftWard : ModSystem
+    {
+        public bool patched;
+        public override void StartPre(ICoreAPI api)
+        {
+            if (!patched)
+            {
+                new Harmony("com.ascyst.customizableriftward").PatchAll();
+                patched = true;
+            }
+            string cfgFileName = "CustomizableRiftWard.json";
+            try
+            {
+                RiftWardConfig.Loaded = api.LoadModConfig<RiftWardConfig.RiftWardConfigData>(cfgFileName);
+                if (RiftWardConfig.Loaded == null)
+                {
+                    RiftWardConfig.Loaded = new RiftWardConfig.RiftWardConfigData();
+                    api.StoreModConfig(RiftWardConfig.Loaded, cfgFileName);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                api.Logger.Error("Error loading or saving " + cfgFileName + ": " + ex.Message);
+            }
+        }
+    }
+}
+
 
     // Harmony Patch to modify Vintagestory.GameContent.BlockEntityRiftWard's private void BlockEntityRiftWard_OnRiftSpawned(Rift rift)
     [HarmonyPatch(typeof(BlockEntityRiftWard), "BlockEntityRiftWard_OnRiftSpawned")]
     public class BlockEntityRiftWard_OnRiftSpawned_Patch
     {
-        // Create a static method that will be called when the patch is applied
+
+        // Prefix because I'm way too lazy to figure out transpiling right now
         static bool Prefix(BlockEntityRiftWard __instance, Rift rift)
         {
+
+
             var hasFuelProperty = AccessTools.Property(typeof(BlockEntityRiftWard), "HasFuel");
             bool hasFuel = (bool)hasFuelProperty.GetValue(__instance);
             var sapiField = AccessTools.Field(typeof(BlockEntityRiftWard), "sapi");
@@ -43,8 +73,11 @@ namespace CustomizableRiftWard
 
             var riftsBlockedField = AccessTools.Field(typeof(BlockEntityRiftWard), "riftsBlocked");
             int riftsBlocked = (int)riftsBlockedField.GetValue(__instance);
-            // If rift is spawned and the riftward has fuel and the rift is within 30 blocks of the riftward
-            if (hasFuel && sapi.World.Rand.NextDouble() <= RiftWardConfig.riftBlockChance && rift.Position.DistanceTo((double)__instance.Pos.X + 0.5, (double)(__instance.Pos.Y + 1), (double)__instance.Pos.Z + 0.5) < RiftWardConfig.maxRange)
+
+            double riftBlockChance = RiftWardConfig.Loaded.riftBlockChance;
+            float maxRange = RiftWardConfig.Loaded.maxRange;
+
+            if (hasFuel && sapi.World.Rand.NextDouble() <= riftBlockChance && rift.Position.DistanceTo((double)__instance.Pos.X + 0.5, (double)(__instance.Pos.Y + 1), (double)__instance.Pos.Z + 0.5) < maxRange)
             {
                 rift.Size = 0f;
                 riftsBlocked++;
@@ -54,8 +87,3 @@ namespace CustomizableRiftWard
             return false;
         }
     }
-
-
-
-
-}
